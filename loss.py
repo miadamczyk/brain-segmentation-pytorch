87cedbfa04e1cd3dba09322c48cb196433b82360
miadamczyk <133974024+miadamczyk@users.noreply.github.com>
2024-11-03 16:59:32 +0100

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
-
+import numpy as np
+import torch
 
 class DiceLoss(nn.Module):
 
@@ -7,12 +8,12 @@ class DiceLoss(nn.Module):
         super(DiceLoss, self).__init__()
         self.smooth = 1.0
 
-    def forward(self, y_pred, y_true):
-        assert y_pred.size() == y_true.size()
-        y_pred = y_pred[:, 0].contiguous().view(-1)
-        y_true = y_true[:, 0].contiguous().view(-1)
-        intersection = (y_pred * y_true).sum()
-        dsc = (2. * intersection + self.smooth) / (
-            y_pred.sum() + y_true.sum() + self.smooth
-        )
-        return 1. - dsc
+    def forward(self, y, lbl):
+        criterion = nn.MSELoss(reduction="mean")
+        criterion2 = nn.BCEWithLogitsLoss(reduction="mean")
+        veci = 5. * torch.from_numpy(lbl[:, 1:]).cuda()
+        loss = criterion(y[:, :2], veci)
+        loss /= 2.
+        loss2 = criterion2(y[:, -1], torch.from_numpy(lbl[:, 0] > 0.5)..cuda().float())
+        loss = loss + loss2
+        return loss
